8632e9cf21edaee006872fd299afc35f71f234ef
Prshkv <niko.prshkv@gmail.com>
2025-04-27 20:24:43 +0300

    s

--- src/main/java/com/prshkv/backendproject/BackendprojectApplication.java
+++ src/main/java/com/prshkv/backendproject/BackendprojectApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.prshkv.backendproject.domain.AppUser;
 import com.prshkv.backendproject.domain.AppUserRepository;
 
 @SpringBootApplication
@@ -18,8 +19,15 @@ public class BackendprojectApplication {
 	@Bean
 	public CommandLineRunner cml(AppUserRepository appUserRepository) {
 		return (args) -> {
+			
+			AppUser testAdmin = new AppUser();
+			testAdmin.setPasswordHash("$2a$10$4JNnAuMep5O5UBnJq9JrVOrR.6BnrAlbYmS2kKYohnP6vFvlO7Bp.");
+			testAdmin.setEmail("admin@email.com");
+			testAdmin.setRole("ROLE_ADMIN");
+			testAdmin.setUsername("admin");
 
-			appUserRepository.deleteAll();
+			appUserRepository.save(testAdmin);
+
 		};
 	}
 
